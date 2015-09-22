e8068d0a68e3535e2f96a526340b00d6804a2df3
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-22 12:00:59 +0100

    passing create links test

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,17 @@ class BookmarkManager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/links/new' do
+    # @links = Link.all
+    erb :'links/new'
+  end
+
+  post '/links/new' do
+    Link.first_or_create(params)
+    redirect '/links'
+  end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
